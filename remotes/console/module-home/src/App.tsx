d53d3cc45644bfda34dca50820129a19074dbe0c
xiaoYown <qzy09101018@sina.com>
2023-03-13 16:13:04 +0800

    feat: 修改 antd 自定义主题样式

--- remotes/console/module-home/src/App.tsx
+++ remotes/console/module-home/src/App.tsx
@@ -1,9 +1,30 @@
+import { Col, Row } from 'antd';
 import './App.css';
 
 const App = () => {
+  const themes = Array.from(Array(10), (_v, k) => k + 1);
   return (
     <>
-      <div className="App">Home</div>
+      <div className="App">
+        <div style={{ width: '400px', padding: '10px' }}>
+          {themes.map(item => {
+            return (
+              <Row key={item}>
+                <Col span={8}>{`--soc-blue-${item}`}</Col>
+                <Col
+                  span={16}
+                  style={{
+                    height: '100%',
+                    backgroundColor: `var(--soc-blue-${item})`,
+                  }}
+                >
+                  &nbsp;
+                </Col>
+              </Row>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
